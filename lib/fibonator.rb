a5b04d2f2d730c73a5df46c45a4ec474bff0e672
Novak <novak_is@live.com>
2020-09-25 21:21:37 +0200

    Add basic functionality in 'naive way'

--- lib/fibonator.rb
+++ lib/fibonator.rb
@@ -1,6 +1,28 @@
 require "fibonator/version"
+require "fibonator/calculate"
 
 module Fibonator
-  class Error < StandardError; end
-  # Your code goes here...
+  extend self
+
+  SOFT_LIMIT = 30000
+
+  def nth_element(nth, soft_limit: SOFT_LIMIT)
+    raise ArgumentError, 'Only numbers are allowed' unless valid_argument?(nth)
+
+    nth = nth.to_i if nth.is_a?(String)
+    raise ArgumentError, "Number too big. Limit set to: #{soft_limit}" if out_of_limit?(nth, soft_limit)
+
+    Fibonator::Calculate.nth_element(nth)
+  end
+
+  private
+
+  def valid_argument?(arg)
+    arg.is_a?(Integer) || (arg == arg.to_i.to_s rescue false)
+  end
+
+  def out_of_limit?(arg, limit)
+    limit.positive? && arg > limit
+  end
+
 end
