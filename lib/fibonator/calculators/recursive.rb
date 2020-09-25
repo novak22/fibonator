ec46797382de35f21f06a2194a36894f2c79275b
Novak <novak_is@live.com>
2020-09-25 22:39:14 +0200

    Add Recursive calculator

---
+++ lib/fibonator/calculators/recursive.rb
@@ -0,0 +1,26 @@
+module Calculators
+  class Recursive
+    attr_accessor :nth_array
+
+    def initialize
+      @nth_array = {0 => 0, 1 => 1, 2 => 1}
+    end
+
+
+    def call(nth)
+      return @nth_array[nth] if @nth_array.has_key?(nth)
+
+      return calculate_nth_naive(nth)
+    end
+
+    private
+    def calculate_nth_naive(nth)
+      return @nth_array[nth] if @nth_array.has_key?(nth)
+
+      el = calculate_nth_naive(nth - 1) + calculate_nth_naive(nth - 2)
+
+      @nth_array[nth] = el
+    end
+
+  end
+end
