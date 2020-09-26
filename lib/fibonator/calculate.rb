6a19284fc7123971175e6b5c74b2f834d6e9b052
Novak <novak_is@live.com>
2020-09-26 13:15:59 +0200

    Add MatrixCalculator as default

--- lib/fibonator/calculate.rb
+++ lib/fibonator/calculate.rb
@@ -1,9 +1,26 @@
 require_relative 'calculators/recursive_calculator'
+require_relative 'calculators/matrix_calculator'
 
 module Fibonator
   class Calculate
-    def self.nth_element nth
-      Calculators::RecursiveCalculator.new.call(nth)
+    def self.nth_element(nth, calculator: :matrix)
+      calculator = calculator_class_from_symbol(calculator)
+      raise ArgumentError, "Invalid calculator" unless calculator
+
+      calculator.call(nth)
+    end
+
+    private
+
+    def self.calculator_class_from_symbol(calculator_symbol)
+      case calculator_symbol
+      when :matrix
+        Calculators::MatrixCalculator.new
+      when :recursive
+        Calculators::RecursiveCalculator.new
+      else
+        false
+      end
     end
   end
 end
