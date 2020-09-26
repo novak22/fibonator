84169365ef27cd9ec81b618d206fbb0c24b5e8ee
Novak <novak_is@live.com>
2020-09-26 12:37:26 +0200

    Remove unused variable

--- lib/fibonator/calculate.rb
+++ lib/fibonator/calculate.rb
@@ -2,8 +2,6 @@ require_relative 'calculators/recursive_calculator'
 
 module Fibonator
   class Calculate
-    @nth_array = {0 => 0, 1 => 1, 2 => 1}
-
     def self.nth_element nth
       Calculators::RecursiveCalculator.new.call(nth)
     end
